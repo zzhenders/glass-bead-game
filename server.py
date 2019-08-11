1d567266c2384598e6394e18bbc066aa4525c837
Zoe Henderson <zzhenders@gmail.com>
2019-08-11 13:31:37 -0700

    Fixed indentation, made connect_to_db call more specific.

--- server.py
+++ server.py
@@ -11,61 +11,61 @@ app = Flask(__name__)
 
 @app.route("/users")
 def users():
-	"""All users."""
+    """All users."""
 
 @app.route("/users/create", methods=['POST'])
 def create_user():
-	"""Add a user."""
+    """Add a user."""
 
 @app.route("/users/<userid>")
 def user_root():
-	"""User root directory and information."""
+    """User root directory and information."""
 
 @app.route("/users/<userid>/update", methods=['POST'])
 def update_user():
-	"""Update user information."""
+    """Update user information."""
 
 @app.route("/users/<userid>/delete", methods=['POST'])
 def delete_user():
-	"""Remove user."""
+    """Remove user."""
 
 @app.route("/users/<userid>/bookmarks")
 def bookmarks():
-	"""Bookmarks a user has saved."""
+    """Bookmarks a user has saved."""
 
 @app.route("/users/<userid>/followers")
 def user_followers():
-	"""Users following user specified."""
+    """Users following user specified."""
 
 @app.route("/users/<userid>/followers/recent-posts")
 def user_followers_recent():
-	"""Most recent posts by user's followers."""
+    """Most recent posts by user's followers."""
 
 @app.route("/users/<userid>/following")
 def user_following():
-	"""Users followed by user specified."""
+    """Users followed by user specified."""
 
 @app.route("/users/<userid>/following/recent-posts")
 def user_following_recent():
-	"""Most recent posts by users followed by user."""
+    """Most recent posts by users followed by user."""
 
 @app.route("/posts")
 def posts():
-	"""I don't know yet but this seems important to have."""
+    """I don't know yet but this seems important to have."""
 
 @app.route("/posts/search")
 def search_posts():
-	"""Search for posts matching given criteria."""
+    """Search for posts matching given criteria."""
 
-	search_terms = request.args.get("terms")
-	list_of_posts = model.Post.search_for(search_terms)  #list of Post objects 
-	dict_of_posts = model.translate_posts_to_dict(list_of_posts)
-	return jsonify(dict_of_posts)
+    search_terms = request.args.get("terms")
+    list_of_posts = model.Post.search_for(search_terms)  #list of Post objects 
+    dict_of_posts = model.translate_posts_to_dict(list_of_posts)
+    return jsonify(dict_of_posts)
 
 
 @app.route("/posts/create", methods=['POST'])
 def create_post():
-	"""Create a new post."""
+    """Create a new post."""
 
     title = request.form.get('title')
     content = request.form.get('content')
@@ -81,28 +81,28 @@ def create_post():
 
 @app.route("/posts/<postid>")
 def post():
-	"""A particular post."""
+    """A particular post."""
 
 @app.route("/posts/<postid>/edit", methods=['POST'])
 def edit_post():
-	"""Update a post."""
+    """Update a post."""
 
 @app.route("/posts/<postid>/erase", methods=['POST'])
 def erase_post():
-	"""Void a post.
+    """Void a post.
 
-	Deletes content of a post. References will still exist until changed by
-	the relevant users.
-	"""
+    Deletes content of a post. References will still exist until changed by
+    the relevant users.
+    """
 
 @app.route("/posts/<postid>/responses")
 def responses():
-	"""Responses to a particular post."""
+    """Responses to a particular post."""
 
 @app.route("/posts/<postid>/origins")
 def origins():
-	"""Origins of a particular post."""
+    """Origins of a particular post."""
 
 if __name__ == '__main__':
-    model.connect_to_db(app)
+    model.connect_to_db(app, model.DB_URI)
     app.run(debug=True, host="0.0.0.0")
