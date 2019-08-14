2c94b6420ff703827be31bbe71287fb4466289ee
Zoe Henderson <zzhenders@gmail.com>
2019-08-14 14:40:31 -0700

    Added boolean on User to allow for user deletion.

--- model.py
+++ model.py
@@ -70,6 +70,7 @@ class User(db.Model):
 
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 	uname = db.Column(db.String(30), nullable=False, unique=True)
+	deleted = db.Column(db.Boolean, nullable=False, default=False)
 
 	# Relationships 
 	bookmarks = db.relationship('Post', secondary="bookmarks")
