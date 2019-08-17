4bee65ce8f3f7b460f1229e6066dcbf4498a334d
Zoe Henderson <zzhenders@gmail.com>
2019-08-17 14:06:59 -0700

    Refactored to use closure.

--- static/bead.js
+++ static/bead.js
@@ -1,16 +1,18 @@
-function panelPostsHandler(results, direction) {
-	let posts = '';
-	Object.entries(results).forEach(([key, post]) => {
-		posts +=
-			`<section class="post-tile">
-				<a href="/bead?postid=${post.id}">
-					<h1>${post.title}</h1>
-				</a>
-				<p>${post.content}</p>
-			</section>`
-		;	
-	});
-	$(direction).html(posts)
+function panelPostsHandler(direction) {
+	return function (results) {
+		let posts = '';
+		Object.entries(results).forEach(([key, post]) => {
+			posts +=
+				`<section class="post-tile">
+					<a href="/bead?postid=${post.id}">
+						<h1>${post.title}</h1>
+					</a>
+					<p>${post.content}</p>
+				</section>`
+			;	
+		});
+		$(direction).html(posts)
+	}
 }
 
 function extendedPostHandler(results) {
@@ -22,6 +24,6 @@ function extendedPostHandler(results) {
 	$('.view').html(extPost)
 }
 
-$.get(`/posts/${postid}/references`, (results) => {panelPostsHandler(results, '.references')});
-$.get(`/posts/${postid}`, (results) => {extendedPostHandler(results)});
-$.get(`/posts/${postid}/responses`, (results) => {panelPostsHandler(results, '.responses')});
+$.get(`/posts/${postid}/references`, panelPostsHandler('.references'));
+$.get(`/posts/${postid}`, extendedPostHandler);
+$.get(`/posts/${postid}/responses`, panelPostsHandler('.responses'));
