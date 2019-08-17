7749aeb4647a7435c684e8a95b75e7158b1a4c48
Zoe Henderson <zzhenders@gmail.com>
2019-08-17 08:40:27 -0700

    Added abstract Aggregate and Bead components.

--- static/App.jsx
+++ static/App.jsx
@@ -46,42 +46,58 @@ class Main extends React.Component {
 	}
 
 	render() {
-		let posts = [];
-		this.props.posts.forEach((post) => {
-			posts.push(
-				<Post
-					key={post.id}
-					title={post.title}
-					content={post.content}
-				/>
-			);	
-		});
 		switch (this.state.page) {
 			case "aggregate":
 				return (
-					<div className="aggregate" id="main">
-						<div className="view flex-flow-row-wrap">
-							{posts}
-						</div>
-					</div>
+					<Aggregate posts={this.props.posts}/>
 				);
 				break;
 
 			case "bead":		
 				return (
-					<div className="bead" id="main">
-						<Panel />
-						<div className="view">
-							<section className="post-extended">
-								<h1>There is a post here</h1>
-								<p>Paragraph here</p>
-							</section>
-						</div>
-						<Panel />
-					</div>
+					<Bead />
 				);
 				break;
 		}
+	}
+}
+
+class Aggregate extends React.Component {
+	render() {
+		let posts = [];
+		Object.entries(this.props.posts).forEach(([key, post]) => {
+			posts.push(
+				<Post
+					key={key}
+					title={post.title}
+					content={post.content}
+				/>
+			);	
+		});
+		return (
+			<div className="aggregate" id="main">
+				<div className="view flex-flow-row-wrap">
+					{posts}
+				</div>
+			</div>
+		);
+	}
+}
+
+class Bead extends React.Component {
+	render() {
+		return(
+			<div className="bead" id="main">
+				<Panel />
+				<div className="view">
+					<section className="post-extended">
+						<h1>There is a post here</h1>
+						<p>Paragraph here</p>
+					</section>
+				</div>
+				<Panel />
+			</div>
+		);
 	}
 }
 
@@ -109,36 +125,36 @@ class Post extends React.Component {
 	}
 }
 
-const posts_static = [
-	{
+const posts_static = {
+	1: {
 		id: 1,
 		title: "Poor Lemongrab",
 		content: "You try your best",
 		user_id: 2,
 		created: 1,
 	},
-	{
+	2: {
 		id: 2,
 		title: "One Million Years Dungeon!!",
 		content: "AaAAaaaAAaaaAaaaaAAAaAaaa",
 		user_id: 1,
 		created: 1,
 	},
-	{
+	3: {
 		id: 3,
 		title: "Here's a song I just wrote",
 		content: "Song song song song song song song",
 		user_id: 3,
 		created: 1,
 	},
-	{
+	4: {
 		id: 4,
 		title: "Gunther",
 		content: "You are very very bad!",
 		user_id: 4,
 		created: 1,
 	}
-]
+}
 
 ReactDOM.render(
 	<App posts={posts_static} />,
