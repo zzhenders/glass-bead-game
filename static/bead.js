dbc6ed78011bf3aa4812c24149b8428f4a16c50f
Zoe Henderson <zzhenders@gmail.com>
2019-08-19 21:27:28 -0700

    Refactored to use fetch. Now gets usernames.

--- static/bead.js
+++ static/bead.js
@@ -1,45 +1,52 @@
 window.UserIds = new Set()
 
-function panelPostsHandler(direction) {
-	return function (results) {
+function panelPostsHandler(url='', direction='') {
+	return fetch(url, {method: 'GET'})
+	.then(response => {return response.json()}).then(data => {
+		// console.log(data);
 		let posts = '';
-		Object.entries(results).forEach(([key, post]) => {
+		Object.entries(data).forEach(([key, post]) => {
+			// console.log(key, post);
 			posts +=
 				`<section class="post-tile">
 					<a href="/bead?postid=${post.id}">
 						<h1>${post.title}</h1>
 					</a>
 					<p>${post.content}</p>
-					<b id="u${post.user_id}">${post.user_id}</b>
-				</section>`
-			;
+					<b class="u${post.user_id}">${post.user_id}</b>
+				</section>`;
 			UserIds.add(post.user_id);	
 		});
-		$(direction).html(posts)
-	}
+		$(direction).html(posts);
+	});
 }
 
-function extendedPostHandler(results) {
-	const extPost =
-			`<section class="post-extended">
-				<h1>${results.title}</h1>
-				<p>${results.content}</p>
-				<b>${results.user_id}</b>
-				<a href="/editpost?postid=${postid}">edit</a>
-			</section>`;
-	$('.view').html(extPost);
-	UserIds.add(results.user_id);
+function extendedPostHandler(url) {
+	return fetch(url, {method: 'GET'})
+	.then(response => {return response.json()}).then(data => {
+		console.log(data);
+		const extPost =
+				`<section class="post-extended">
+					<h1>${data.title}</h1>
+					<p>${data.content}</p>
+					<b class="u${data.user_id}">${data.user_id}</b>
+					<a href="/editpost?postid=${data.id}">edit</a>
+				</section>`;
+		$('.view').html(extPost);
+		UserIds.add(data.user_id);
+	});
 }
 
-$.get(`/posts/${postid}/references`, panelPostsHandler('.references'));
-$.get(`/posts/${postid}`, extendedPostHandler);
-$.get(`/posts/${postid}/responses`, panelPostsHandler('.responses'));
-$.get('/users/unames',
-		   {'userids': Array.from(UserIds).join('.')},
-		   (response) => {
-		   		console.log(response);
-				Object.entries(response).forEach(([key, user]) => {
-					$($`u{user.id}`).html(user.uname);
-				}
-				);
-			});
+function getUsernames() {
+	return fetch(`/users/unames?userids=${Array.from(UserIds).join('.')}`)
+	.then(response => {return response.json()}).then(data => {
+		Object.entries(data).forEach(([key, user]) => {
+					$(`.u${key}`).html(user);});
+	});
+}
+
+a = panelPostsHandler(`/posts/${postid}/references`, '.references');
+b = panelPostsHandler(`/posts/${postid}/responses`, '.responses');
+c = extendedPostHandler(`/posts/${postid}`)
+
+Promise.all([a, b, c]).then(getUsernames);
