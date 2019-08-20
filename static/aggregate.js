9de7b3f1b47a5cab3d09639451ae1e2a5e58d58a
Zoe Henderson <zzhenders@gmail.com>
2019-08-20 16:34:00 -0700

    Refactored to use promises, now gets usernames.

--- static/aggregate.js
+++ static/aggregate.js
@@ -1,17 +1,33 @@
-function aggregatePostsHandler(results){
-	let posts = '';
-	Object.entries(results).forEach(([key, post]) => {
-		posts +=
-			`<section class="post">
-				<a href="/bead?postid=${post.id}">
-					<h1>${post.title}</h1>
-				</a>
-				<p>${post.content}</p>
-				<b>${post.user_id}</b>
-			</section>`
-		;	
+window.UserIds = new Set()
+
+function aggregatePostsHandler(api='') {
+	return fetch(api, {method: 'GET'})
+	.then(response => {return response.json()}).then(data => {
+		// console.log(data);
+		let posts = '';
+		Object.entries(data).forEach(([key, post]) => {
+			// console.log(key, post);
+			posts +=
+				`<section class="post">
+					<a href="/bead?postid=${post.id}">
+						<h1>${post.title}</h1>
+					</a>
+					<p>${post.content}</p>
+					<b class="u${post.user_id}">${post.user_id}</b>
+					<i id="bookmarker pb${post.id}"></i>
+				</section>`;
+			UserIds.add(post.user_id);
+		});
+		$('#main').html(posts);
 	});
-	$('#main').html(posts)
 }
 
-$.get(api, (results) => {aggregatePostsHandler(results)});
+function getUsernames() {
+	return fetch(`/users/unames?userids=${Array.from(UserIds).join('.')}`)
+	.then(response => {return response.json()}).then(data => {
+		Object.entries(data).forEach(([key, user]) => {
+					$(`.u${key}`).html(user);});
+	});
+}
+
+aggregatePostsHandler(api).then(getUsernames)
