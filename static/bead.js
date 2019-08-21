06e0c5c01362a34b2e476b9a41cc28d0356bc9cd
Zoe Henderson <zzhenders@gmail.com>
2019-08-21 14:24:01 -0700

    Added bookmarker functionality.

--- static/bead.js
+++ static/bead.js
@@ -1,4 +1,5 @@
-window.UserIds = new Set()
+window.UserIds = new Set();
+window.PostIds = new Set();
 
 function panelPostsHandler(url='', direction='') {
 	return fetch(url, {method: 'GET'})
@@ -34,10 +35,54 @@ function extendedPostHandler(url) {
 					<a href="aggregate?api=.users.${data.user_id}.posts"
 					class="u${data.user_id}"></a>
 					<a href="/editpost?postid=${data.id}">edit</a>
+					<i class="bookmarker"
+					id="pb${data.id}"
+					post-id="${data.id}"></i>
 				</section>`;
 		$('.view').html(extPost);
 		UserIds.add(data.user_id);
+		PostIds.add(data.id);
 	});
+}
+
+function bookmarker(evt) {
+	post_id = evt.target.attributes['post-id'].value;
+	if (evt.target.innerHTML === "unbookmark") {
+		return fetch(`/users/${uid}/bookmarks/delete`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			body: `post_id=${post_id}`,
+		}).then(() => $(`#pb${post_id}`).html('bookmark'));
+	} else if (evt.target.innerHTML === "bookmark") {
+		return fetch(`/users/${uid}/bookmarks/create`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			body: `post_id=${post_id}`,
+		}).then(() => $(`#pb${post_id}`).html('unbookmark'));
+	}
+}
+
+function getBookmarks() {
+	if (PostIds.size != 0) {
+		uid = parseInt(document.cookie.slice(4));
+		return fetch(`/posts/bookmarked?postids=${Array.from(PostIds).join('.')}&uid=${uid}`)
+		.then(response => {return response.json()})
+		.then(data => {
+			Object.entries(data).forEach(([key, value]) => {
+				if (value) {
+					console.log(value);
+					$(`#pb${key}`).html('unbookmark');
+				} else {
+					$(`#pb${key}`).html('bookmark');
+				}
+				$(`#pb${key}`).click(bookmarker);
+			})
+		})
+	}
 }
 
 function getUsernames() {
@@ -57,4 +102,4 @@ b = panelPostsHandler(`/posts/${postid}/responses`, '.responses');
 c = extendedPostHandler(`/posts/${postid}`)
 
 Promise.all([a, b, c])
-.then(getUsernames)
+.then(getUsernames).then(getBookmarks);
