6865a41c94c8d4bfeaaa8a1203151428a8dd3743
Zoe Henderson <zzhenders@gmail.com>
2019-08-24 15:10:53 -0700

    Aggregate moved to module.

---
+++ gb-react/src/Aggregate.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import Post from './Post';
+import { getPosts } from './Api';
+
+class Aggregate extends React.Component {
+	constructor(props) {
+	    super(props);
+	    this.state = {
+	    	posts: new Object(),
+	    	isLoaded: false,
+	    };
+	}
+
+	componentDidMount() {
+		getPosts(this.props.data).then(
+			(data) => {
+				this.setState({
+					posts: data,
+					isLoaded: true,
+				});
+			}
+		);
+	}
+
+	componentDidUpdate(prevProps) {
+		if (prevProps.data != this.props.data) { 
+			getPosts(this.props.data).then(
+				(data) => {
+					this.setState({
+						posts: data,
+						isLoaded: true,
+					});
+				}
+			);
+		}
+	}
+
+	render() {
+		let posts = [];
+		Object.entries(this.state.posts).forEach(([key, post]) => {
+			posts.push(
+				<Post
+					uid={this.props.uid}
+					key={key}
+					post_id={post.id}
+					title={post.title}
+					content={post.content}
+					user_id={post.user_id}
+				/>
+			);	
+		});
+		return (
+			<div className="aggregate" id="main">
+				<div className="view flex-flow-row-wrap">
+					{posts}
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Aggregate;
