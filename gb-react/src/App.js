52fdbd79a728a09cd990bdb628198c1ff961690c
Zoe Henderson <zzhenders@gmail.com>
2019-08-24 14:23:09 -0700

    Replaced React's default App component with Glassbeads App.

--- gb-react/src/App.js
+++ gb-react/src/App.js
@@ -2,26 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './Api';
 import './App.css';
+import './Navbar';
+import './Main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+
+      this.state = {
+        data: "/posts/search?terms=e",
+        page: "aggregate",
+        uid: 2,
+      };
+      this.setView = this.setView.bind(this);
+    }
+
+    setView(viewPage, viewData) {
+      return () => {
+        this.setState(state => ({
+          page: viewPage,
+          data: viewData,
+        }));
+      }
+    } 
+
+  render() {
+    return (
+      <div id="app">
+        <Navbar setView={this.setView} uid={this.state.uid}/>
+        <Main setView={this.setView} page={this.state.page} data={this.state.data} uid={this.state.uid}/>
+      </div>
+    );
+  }
 }
 
 export default App;
