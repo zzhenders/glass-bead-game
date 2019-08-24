da247cc11b129a2308bfab55dede11f24b2e3851
Zoe Henderson <zzhenders@gmail.com>
2019-08-24 14:12:32 -0700

    Added getPosts api call fn.

--- gb-react/src/Api.js
+++ gb-react/src/Api.js
@@ -1,12 +1,21 @@
 let apiBase = 'http://localhost:5000'
-
 if (process.env.NODE_ENV === 'production') {
 	apiBase = '';
 }
 
+export function getPosts(api) {
+	let uri = `${apiBase}${api}`
+	console.log(uri)
+	return fetch(uri, {
+	}).then(response => {
+		console.log(response);
+		return response.json()
+	});
+}
+
 export function updateBookmark(uid, post_id, action) {
 	return () => {
-		uri = `${apiBase}/users/${uid}/bookmarks/${action}`
+		let uri = `${apiBase}/users/${uid}/bookmarks/${action}`
 		fetch(uri, {
 			method: 'POST',
 			headers: {
