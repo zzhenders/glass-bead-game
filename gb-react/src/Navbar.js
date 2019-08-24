d1123bcd68740c2d3fec94a2f604eaa2ebd0dacc
Zoe Henderson <zzhenders@gmail.com>
2019-08-24 14:39:11 -0700

    Moved Navbar to module.

---
+++ gb-react/src/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from './logo.svg';
+import './NavButton';
+
+class Navbar extends React.Component {
+	constructor(props) {
+	    super(props);
+
+	    this.state = {
+	    	isShowSearch: false,
+	    };
+  	}
+
+  	toggleShowSearch = () => {
+  		this.setState(state => ({
+  			isShowSearch: !state.isShowSearch
+  		}));
+  	};
+
+  	setView = (page, data) => {
+  		return this.props.setView(page, data);
+  	}
+
+  	render() {
+  		const uid = this.props.uid;
+		return (
+			<nav>
+				<NavButton id="home-btn" alt="Home" onClick={this.setView('aggregate', `/users/${uid}/following/recent-posts`)} />
+				<NavButton id="bookmarks-btn" alt="Bookmarks"/>
+				<NavButton id="userroot-btn" alt="Root"/>
+				<NavButton id="add-post-btn" alt="Add"/>
+				<NavButton id="search-btn" alt="Search" onClick={this.toggleShowSearch}/>
+				{this.state.isShowSearch ? <SearchForm /> : null}
+				<NavButton id="settings-btn" alt="Settings"/>
+				<NavButton id="logout-btn" alt="Logout"/>
+			</nav>
+		);
+	}
+}
