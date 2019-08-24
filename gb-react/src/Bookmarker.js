d37dc1e78cd1c640958de50aa3ea5a9643d502b6
Zoe Henderson <zzhenders@gmail.com>
2019-08-24 15:23:32 -0700

    Moved Bookmarker to module.

---
+++ gb-react/src/Bookmarker.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Bookmarker extends React.Component {
+	render() {
+		const uid = this.props.uid;
+		const post_id = this.props.post_id;
+		return (
+			<i className="bookmarker"
+			id="pb${post_id}"
+			onClick={ () => {this.props.onClick(uid, post_id)} }>'hi'
+			</i>
+		);
+	}
+}
+
+export default Bookmarker;
