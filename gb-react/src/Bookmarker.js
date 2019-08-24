ab4af36aa50a6ea731b46e88bf2741b5cba0cd59
Zoe Henderson <zzhenders@gmail.com>
2019-08-24 15:30:38 -0700

    Fixed broken template string

--- gb-react/src/Bookmarker.js
+++ gb-react/src/Bookmarker.js
@@ -6,7 +6,7 @@ class Bookmarker extends React.Component {
 		const post_id = this.props.post_id;
 		return (
 			<i className="bookmarker"
-			id="pb${post_id}"
+			id="pb{post_id}"
 			onClick={ () => {this.props.onClick(uid, post_id)} }>'hi'
 			</i>
 		);
