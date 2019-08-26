55a9932d88ac18bd9a2047969f35b8a93711bd5c
Zoe Henderson <zzhenders@gmail.com>
2019-08-26 14:04:39 -0700

    Added lookupUsernames method.

--- gb-react/src/Bead.js
+++ gb-react/src/Bead.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import Panel from './Panel';
+import { getUsernames } from './Api';
 
 class Bead extends React.Component {
+	constructor(props) {
+	    super(props);
+	    this.state = {
+	    	users: new Object(),
+	    	isLoaded: false,
+	    };
+	    this.lookupUsernames = this.lookupUsernames.bind(this);
+	}
+
+	lookupUsernames(arrayOfUsernames) {
+		getUsernames(arrayOfUsernames).then(data => {
+		})
+	};
+
 	render() {
 		return(
 			<div className="bead" id="main">
