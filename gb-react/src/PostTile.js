f5ea708e516b63c97f34912766ff907581417a52
Zoe Henderson <zzhenders@gmail.com>
2019-08-26 16:39:19 -0700

    Added onClick to setView, included User component.

--- gb-react/src/PostTile.js
+++ gb-react/src/PostTile.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import User from './User';
 
 class PostTile extends React.Component {
 	render() {
+		let users = this.props.users;
+		let post_id = this.props.post_id;
 		return (
-			<section className="post-tile">
+			<section
+				className="post-tile"
+				onClick={
+					() => {
+						console.log(post_id);
+						this.props.setView('bead', post_id);
+					}}>
 				<h1>{this.props.title}</h1>
 				<p>{this.props.content}</p>
+				{ this.props.beadIsLoaded
+					? <User
+						user_id={this.props.user_id}
+						users={this.props.users}
+						/>
+					: null }
 			</section>
 		);
 	}
