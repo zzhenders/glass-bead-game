80bd7cf80fb52b6421b29fb75f6b96839684a8a5
Zoe Henderson <zzhenders@gmail.com>
2019-08-26 14:05:01 -0700

    Added getUsernames function.

--- gb-react/src/Api.js
+++ gb-react/src/Api.js
@@ -11,15 +11,22 @@ export function getPosts(api) {
 	});
 }
 
+export function getUsernames(arrayOfUsernames) {
+
+	let uri = `${apiBase}/users/unames?userids=${arrayOfUsernames.join('.')}`
+	return fetch(uri)
+	.then(response => {
+		return response.json()
+	});
+}
+
 export function updateBookmark(uid, post_id, action) {
-	return () => {
-		let uri = `${apiBase}/users/${uid}/bookmarks/${action}`
-		fetch(uri, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-			},
-			body: `post_id=${post_id}`,
-  		});
-	}
+	let uri = `${apiBase}/users/${uid}/bookmarks/${action}`
+	return fetch(uri, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		body: `post_id=${post_id}`,
+		});
 }
