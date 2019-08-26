9cd30c5919061da227119bafda671e57257194fa
Zoe Henderson <zzhenders@gmail.com>
2019-08-26 16:20:42 -0700

    Main now functioning as router, moved bookmark functionality down. Also cleaned up style.

--- gb-react/src/Main.js
+++ gb-react/src/Main.js
@@ -1,61 +1,28 @@
 import React from 'react';
 import Aggregate from './Aggregate';
 import Bead from './Bead';
-import { updateBookmark } from './Api';
 
 class Main extends React.Component {
-	constructor(props) {
-	    super(props);
-	    this.state = {
-	    	bookmarks: new Set(),
-	    };
-	    this.setBookmarker = this.setBookmarker.bind(this);
-	    this.addBookmark = this.addBookmark.bind(this);
-	    this.removeBookmark = this.removeBookmark.bind(this);
-	    this.isBookmarked = this.isBookmarked.bind(this);
-  	}
-
-  	setBookmarker = (uid, post_id) => {
-		if (this.isBookmarked(post_id)) {
-			updateBookmark(uid, post_id, 'delete')
-			.then(this.removeBookmark(post_id));
-		} else {
-			updateBookmark(uid, post_id, 'create')
-			.then(this.addBookmark(post_id));
-		}
-  	}
-
-  	addBookmark(post_id) {
-  		this.setState(({bookmarks}) => {
-  			bookmarks: new Set(bookmarks).add(post_id)
-  		});
-  	}
-
-  	removeBookmark(post_id) {
-  		this.setState(({bookmarks}) => {
-  			const newBookmarks = new Set(bookmarks);
-  			newBookmarks.delete(post_id);
-  			return {
-  				bookmarks: newBookmarks
-  			};
-  		});
-  	}
-
-  	isBookmarked(post_id) {
-  		return this.state.bookmarks.has(post_id);
-  	}
 
 	render() {
 		switch (this.props.page) {
 			case "aggregate":
 				return (
-					<Aggregate uid={this.props.uid} data={this.props.data} bookmarker={this.setBookmarker}/>
+					<Aggregate
+						uid={this.props.uid}
+						data={this.props.data}
+						setView={this.props.setView}
+					/>
 				);
 				break;
 
 			case "bead":		
 				return (
-					<Bead data={this.props.data} bookmarker={this.setBookmarker}/>
+					<Bead
+						uid={this.props.uid}
+						data={this.props.data}
+						setView={this.props.setView}
+					/>
 				);
 				break;
 		}
