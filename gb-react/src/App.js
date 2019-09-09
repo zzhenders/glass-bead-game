d62220f9c1392c6736e27c68f54693578640631d
Zoe Henderson <zzhenders@gmail.com>
2019-09-09 16:49:01 -0700

    Importing needed FontAwesome icons.

--- gb-react/src/App.js
+++ gb-react/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faSignInAlt, faUserPlus, faHome, faUser, faEdit, faSearch,
          faBookmark as faBookmarkSolid, faCog, faSignOutAlt,
-         faArrowAltCircleRight, faPlusCircle, faMinusCircle
+         faArrowAltCircleRight, faPlusCircle, faMinusCircle, faReply,
          } from '@fortawesome/free-solid-svg-icons';
 import { faBookmark as faBookmarkRegular } from '@fortawesome/free-regular-svg-icons'; 
 import { checkAuthentication } from './Api';
@@ -12,7 +12,7 @@ import Main from './Main';
 
 library.add(faSignInAlt, faUserPlus, faHome, faBookmarkSolid, faBookmarkRegular,
             faUser, faEdit, faSearch, faCog, faSignOutAlt, faArrowAltCircleRight,
-            faPlusCircle, faMinusCircle)
+            faPlusCircle, faMinusCircle, faReply)
 
 class App extends React.Component {
   constructor(props) {
